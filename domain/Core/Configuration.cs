8eed52fc309f4667644bbc121f033606beb17052
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-05 17:13:11 -0500

    Agregar clase Configuration con manejo de secrets.json

---
+++ domain/Core/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace domain.Core
+{
+    public class Configuration
+    {
+        public static string ConexionString { get; } = "";
+        public static string Key { get; } = "";
+
+        static Configuration()
+        {
+            string JsonPath = "../secrets.json";
+            if (!File.Exists(JsonPath))
+            {
+                Console.Error.WriteLine("The \"secrets.json\" is missed");
+                return;
+            }
+
+            StreamReader jsonStream = File.OpenText(JsonPath);
+            var json = jsonStream.ReadToEnd();
+            var datos = JsonConversor.ToObject<Dictionary<string, string>>(json)!;
+
+            ConexionString = datos["ConexionString"]!;
+            Key = datos["Key"]!;
+        }
+    }
+}
