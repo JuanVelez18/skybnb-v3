244161efe8cfa4a973c803efad2c7d2ca40f7bde
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-05 16:38:32 -0500

    Agregar la interfaz IBaseRepository que contiene los métodos básicos de CRUD

---
+++ repository/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace repository.Interfaces
+{
+    public interface IBaseRepository<TEntity, TKey> where TEntity: class
+    {
+        Task<TEntity?> GetByIdAsync(TKey id);
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
