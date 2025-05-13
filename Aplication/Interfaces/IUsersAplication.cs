38315cee0f6500378ed39710660e2e4ca620e275
Abdreah0 <andreahiguitagallego123@gmail.com>
2025-05-13 10:50:30 -0500

    Agregar interfaces de usuarios para la capa de aplicaciones

---
+++ Aplication/Interfaces/IUsersAplication.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+namespace lib_aplicaciones.Interfaces
+{
+    public interface IRolesAplication
+    {
+        void Configuration(string StringConexion);
+        List<Users> PorEstudiante(Users? entidad);
+        List<Users> Listar();
+        Users? Guardar(Users? entidad);
+        Users? Modificar(Users? entidad);
+        Users? Borrar(Users? entidad);
+    }
+}
