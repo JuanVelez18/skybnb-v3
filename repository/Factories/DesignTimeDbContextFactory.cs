bdd2a92921e8cbeafc846c6a4f26cd731bfeb146
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-14 09:31:07 -0500

    Crear clase de fabrica para DbConexion y configurar User Secrets para el string de conexión

---
+++ repository/Factories/DesignTimeDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using repository.Conexions;
+
+namespace repository.Factories
+{
+    public class DesignTimeDbContextFactory: IDesignTimeDbContextFactory<DbConexion>
+    {
+        public DbConexion CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddUserSecrets<DesignTimeDbContextFactory>()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("ConexionString");
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("I can't find the connection string “ConexionString” in User Secrets");
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<DbConexion>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new DbConexion(optionsBuilder.Options);
+        }
+    }
+}
