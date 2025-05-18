32cc4e88f383624527b56cd639b8622f02f70b97
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-18 15:35:25 -0500

    Agregar implementación de IUserRepository

---
+++ repository/Implementations/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using repository.Conexions;
+using repository.Interfaces;
+
+namespace repository.Implementations
+{
+    public class UserRepository: BaseRepository<Users, Guid>, IUserRepository
+    {
+        public UserRepository(DbConexion conexion) : base(conexion) {}
+
+        public async Task<Users?> GetByEmailAsync(string email)
+        {
+            return await _dbSet
+                .FirstOrDefaultAsync(user => user.Email == email);
+        }
+    }
+}
