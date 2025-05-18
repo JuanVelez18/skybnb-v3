1c49f1f407488d7b98bfcaf79f16c214efee8361
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-18 15:20:55 -0500

    Agregar implementación de IBaseRepository

---
+++ repository/Implementations/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using repository.Conexions;
+using repository.Interfaces;
+
+namespace repository.Implementations
+{
+    public class BaseRepository<TEntity, TKey> : IBaseRepository<TEntity, TKey> where TEntity : class
+    {
+        protected readonly DbConexion _conexion;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public BaseRepository(DbConexion conexion) {
+            _conexion = conexion;
+            _dbSet = _conexion.Set<TEntity>();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (entity is ISoftDeletable softDeletableEntity)
+            {
+                softDeletableEntity.IsActive = false;
+                _dbSet.Update(entity);
+            } else
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+    }
+}
