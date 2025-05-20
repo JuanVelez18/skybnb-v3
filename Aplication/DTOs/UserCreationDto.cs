78f7a689c5b861d5061164b9012ddefc422e61b0
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-20 02:11:19 -0500

    Agregar Dto's para la creación básica de una usuario

---
+++ Aplication/DTOs/UserCreationDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace application.DTOs
+{
+    public class UserCreationDto
+    {
+        public required string Dni { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+        [Required(ErrorMessage = "Birthday is required")]
+        public required DateOnly Birthday { get; set; }
+
+        [Required(ErrorMessage = "CountryId is required")]
+        public required int CountryId { get; set; }
+        public string? Phone { get; set; }
+
+        public UserCredentialsDto ToCredentials() => new()
+        {
+            Email = Email,
+            Password = Password
+        };
+    }
+}
---
+++ Aplication/DTOs/UserCredentialsDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace application.DTOs
+{
+    public class UserCredentialsDto
+    {
+        public static class PasswordValidations
+        {
+            public const string PasswordRegex = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$";
+            public const string PasswordErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character";
+        }
+
+        [Required(ErrorMessage = "User email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        [MaxLength(20, ErrorMessage = "Password must be at most 20 characters long")]
+        [RegularExpression(PasswordValidations.PasswordRegex, ErrorMessage = PasswordValidations.PasswordErrorMessage)]
+        public required string Password { get; set; }
+    }
+}
