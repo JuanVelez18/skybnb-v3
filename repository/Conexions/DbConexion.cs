33c67f4ef32066ca93df14aa3f32d810abc8e853
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-19 23:40:00 -0500

    Centralizar información inicial in InitialData

--- repository/Conexions/DbConexion.cs
+++ repository/Conexions/DbConexion.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq.Expressions;
 using domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using repository.Configuration;
 
 namespace repository.Conexions
 {
@@ -60,62 +61,13 @@ namespace repository.Conexions
                 .WithMany(p => p.Reviews)
                 .OnDelete(DeleteBehavior.ClientCascade);
 
-            // ROLES
-            var adminRole = new Roles("Admin", "Represent the system administrator") { Id = 1 };
-            var hostRole = new Roles("Host", "Represent the property owner") { Id = 2 };
-            var guestRole = new Roles("Guest", "Represent the property guest") { Id = 3 };
-
-            // PERMISSIONS
-            // Country permissions
-            var createCountryPermission = new Permissions("create:country", "Permission to create a country") { Id = 1 };
-            var readCountryPermission = new Permissions("read:country", "Permission to read a country") { Id = 2 };
-            var updateCountryPermission = new Permissions("update:country", "Permission to update a country") { Id = 3 };
-
-            // City permissions
-            var createCityPermission = new Permissions("create:city", "Permission to create a city") { Id = 5 };
-            var readCityPermission = new Permissions("read:city", "Permission to read a city") { Id = 6 };
-            var updateCityPermission = new Permissions("update:city", "Permission to update a city") { Id = 7 };
-
-            // Property type permissions
-            var createPropertyTypePermission = new Permissions("create:propertyType", "Permission to create a property type") { Id = 9 };
-            var readPropertyTypePermission = new Permissions("read:propertyType", "Permission to read a property type") { Id = 10 };
-            var updatePropertyTypePermission = new Permissions("update:propertyType", "Permission to update a property type") { Id = 11 };
-
-            // Address permissions
-            var createAddressPermission = new Permissions("create:address", "Permission to create an address") { Id = 13 };
-            var readAddressPermission = new Permissions("read:address", "Permission to read an address") { Id = 14 };
-            var updateAddressPermission = new Permissions("update:address", "Permission to update an address") { Id = 15 };
-            var deleteAddressPermission = new Permissions("delete:address", "Permission to delete an address") { Id = 16 };
-
-            // Property permissions
-            var createPropertyPermission = new Permissions("create:property", "Permission to create a property") { Id = 17 };
-            var readPropertyPermission = new Permissions("read:property", "Permission to read a property") { Id = 18 };
-            var updatePropertyPermission = new Permissions("update:property", "Permission to update a property") { Id = 19 };
-            var deletePropertyPermission = new Permissions("delete:property", "Permission to delete a property") { Id = 20 };
-
-            // Booking permissions
-            var createBookingPermission = new Permissions("create:booking", "Permission to create a booking") { Id = 21 };
-            var readBookingPermission = new Permissions("read:booking", "Permission to read a booking") { Id = 22 };
-            var updateBookingPermission = new Permissions("update:booking", "Permission to update a booking") { Id = 23 };
-            var deleteBookingPermission = new Permissions("delete:booking", "Permission to delete a booking") { Id = 24 };
-
-            // Review permissions
-            var createReviewPermission = new Permissions("create:review", "Permission to create a review") { Id = 25 };
-            var readReviewPermission = new Permissions("read:review", "Permission to read a review") { Id = 26 };
-            var updateReviewPermission = new Permissions("update:review", "Permission to update a review") { Id = 27 };
-            var deleteReviewPermission = new Permissions("delete:review", "Permission to delete a review") { Id = 28 };
-
-            modelBuilder.Entity<Roles>().HasData(adminRole, hostRole, guestRole);
-
-            modelBuilder.Entity<Permissions>().HasData(
-                createCountryPermission, readCountryPermission, updateCountryPermission,
-                createCityPermission, readCityPermission, updateCityPermission,
-                createPropertyTypePermission, readPropertyTypePermission, updatePropertyTypePermission,
-                createAddressPermission, readAddressPermission, updateAddressPermission, deleteAddressPermission,
-                createPropertyPermission, readPropertyPermission, updatePropertyPermission, deletePropertyPermission,
-                createBookingPermission, readBookingPermission, updateBookingPermission, deleteBookingPermission,
-                createReviewPermission, readReviewPermission, updateReviewPermission, deleteReviewPermission
+            modelBuilder.Entity<Roles>().HasData(
+                InitialData.AdminRole,
+                InitialData.HostRole,
+                InitialData.GuestRole
             );
+
+            modelBuilder.Entity<Permissions>().HasData(InitialData.GetAllPermissions());
 
             // Assign permissions to roles
             modelBuilder.Entity<Roles>()
@@ -128,38 +80,19 @@ namespace repository.Conexions
                     j =>
                     {
                         j.HasKey("RolesId", "PermissionsId");
-                        j.HasData(
-                            // Admin permissions
-                            new { RolesId = adminRole.Id, PermissionsId = createCountryPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = readCountryPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = updateCountryPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = createCityPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = readCityPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = updateCityPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = createPropertyTypePermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = readPropertyTypePermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = updatePropertyTypePermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = updateAddressPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = readPropertyPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = updatePropertyPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = createBookingPermission.Id },
-                            new { RolesId = adminRole.Id, PermissionsId = deleteReviewPermission.Id },
 
-                            // Host permissions
-                            new { RolesId = hostRole.Id, PermissionsId = createPropertyPermission.Id },
-                            new { RolesId = hostRole.Id, PermissionsId = readPropertyPermission.Id },
-                            new { RolesId = hostRole.Id, PermissionsId = updatePropertyPermission.Id },
-                            new { RolesId = hostRole.Id, PermissionsId = deletePropertyPermission.Id },
-                            new { RolesId = hostRole.Id, PermissionsId = updateBookingPermission.Id },
-                            new { RolesId = hostRole.Id, PermissionsId = readBookingPermission.Id },
+                        var rolePermissions = InitialData.GetRolePermissionMappings();
+                        var initialData = new List<object>();
 
-                            // Guest permissions
-                            new { RolesId = guestRole.Id, PermissionsId = createBookingPermission.Id },
-                            new { RolesId = guestRole.Id, PermissionsId = updateBookingPermission.Id },
-                            new { RolesId = guestRole.Id, PermissionsId = readBookingPermission.Id },
-                            new { RolesId = guestRole.Id, PermissionsId = createReviewPermission.Id },
-                            new { RolesId = guestRole.Id, PermissionsId = readReviewPermission.Id }
-                        );
+                        foreach (var roleId in rolePermissions.Keys)
+                        {
+                            foreach (var permissionId in rolePermissions[roleId])
+                            {
+                                initialData.Add(new { RolesId = roleId, PermissionsId = permissionId });
+                            }
+                        }
+
+                        j.HasData(initialData);
                     }
                 );
 
---
+++ repository/Configuration/InitialData.cs
@@ -0,0 +1,89 @@
+﻿using domain.Entities;
+
+namespace repository.Configuration
+{
+    public class InitialData
+    {
+        // ROLES
+        public static readonly Roles AdminRole = new ("Admin", "Represent the system administrator") { Id = 1 };
+        public static readonly Roles HostRole = new ("Host", "Represent the property owner") { Id = 2 };
+        public static readonly Roles GuestRole = new ("Guest", "Represent the property guest") { Id = 3 };
+
+        // PERMISSIONS
+        // Country permissions
+        public static readonly Permissions CreateCountryPermission = new ("create:country", "Permission to create a country") { Id = 1 };
+        public static readonly Permissions ReadCountryPermission = new ("read:country", "Permission to read a country") { Id = 2 };
+        public static readonly Permissions UpdateCountryPermission = new ("update:country", "Permission to update a country") { Id = 3 };
+
+        // City permissions
+        public static readonly Permissions CreateCityPermission = new("create:city", "Permission to create a city") { Id = 5 };
+        public static readonly Permissions ReadCityPermission = new("read:city", "Permission to read a city") { Id = 6 };
+        public static readonly Permissions UpdateCityPermission = new("update:city", "Permission to update a city") { Id = 7 };
+
+        // Property type permissions
+        public static readonly Permissions CreatePropertyTypePermission = new ("create:propertyType", "Permission to create a property type") { Id = 9 };
+        public static readonly Permissions ReadPropertyTypePermission = new ("read:propertyType", "Permission to read a property type") { Id = 10 };
+        public static readonly Permissions UpdatePropertyTypePermission = new ("update:propertyType", "Permission to update a property type") { Id = 11 };
+
+        // Address permissions
+        public static readonly Permissions CreateAddressPermission = new ("create:address", "Permission to create an address") { Id = 13 };
+        public static readonly Permissions ReadAddressPermission = new ("read:address", "Permission to read an address") { Id = 14 };
+        public static readonly Permissions UpdateAddressPermission = new ("update:address", "Permission to update an address") { Id = 15 };
+        public static readonly Permissions DeleteAddressPermission = new ("delete:address", "Permission to delete an address") { Id = 16 };
+
+        // Property permissions
+        public static readonly Permissions CreatePropertyPermission = new ("create:property", "Permission to create a property") { Id = 17 };
+        public static readonly Permissions ReadPropertyPermission = new ("read:property", "Permission to read a property") { Id = 18 };
+        public static readonly Permissions UpdatePropertyPermission = new ("update:property", "Permission to update a property") { Id = 19 };
+        public static readonly Permissions DeletePropertyPermission = new ("delete:property", "Permission to delete a property") { Id = 20 };
+
+        // Booking permissions
+        public static readonly Permissions CreateBookingPermission = new ("create:booking", "Permission to create a booking") { Id = 21 };
+        public static readonly Permissions ReadBookingPermission = new ("read:booking", "Permission to read a booking") { Id = 22 };
+        public static readonly Permissions UpdateBookingPermission = new ("update:booking", "Permission to update a booking") { Id = 23 };
+        public static readonly Permissions DeleteBookingPermission = new ("delete:booking", "Permission to delete a booking") { Id = 24 };
+
+        // Review permissions
+        public static readonly Permissions CreateReviewPermission = new ("create:review", "Permission to create a review") { Id = 25 };
+        public static readonly Permissions ReadReviewPermission = new ("read:review", "Permission to read a review") { Id = 26 };
+        public static readonly Permissions UpdateReviewPermission = new ("update:review", "Permission to update a review") { Id = 27 };
+        public static readonly Permissions DeleteReviewPermission = new ("delete:review", "Permission to delete a review") { Id = 28 };
+
+        public static List<Permissions> GetAllPermissions()
+        {
+            return [
+                CreateCountryPermission, ReadCountryPermission, UpdateCountryPermission,
+                CreateCityPermission, ReadCityPermission, UpdateCityPermission,
+                CreatePropertyTypePermission, ReadPropertyTypePermission, UpdatePropertyTypePermission,
+                CreateAddressPermission, ReadAddressPermission, UpdateAddressPermission, DeleteAddressPermission,
+                CreatePropertyPermission, ReadPropertyPermission, UpdatePropertyPermission, DeletePropertyPermission,
+                CreateBookingPermission, ReadBookingPermission, UpdateBookingPermission, DeleteBookingPermission,
+                CreateReviewPermission, ReadReviewPermission, UpdateReviewPermission, DeleteReviewPermission
+            ];
+        }
+
+        public static Dictionary<int, List<int>> GetRolePermissionMappings()
+        {
+            return new Dictionary<int, List<int>>
+            {
+                [AdminRole.Id] = [
+                    CreateCountryPermission.Id, ReadCountryPermission.Id, UpdateCountryPermission.Id,
+                    CreateCityPermission.Id, ReadCityPermission.Id, UpdateCityPermission.Id,
+                    CreatePropertyTypePermission.Id, ReadPropertyTypePermission.Id, UpdatePropertyTypePermission.Id,
+                    UpdateAddressPermission.Id,
+                    ReadPropertyPermission.Id, UpdatePropertyPermission.Id,
+                    CreateBookingPermission.Id,
+                    DeleteReviewPermission.Id
+                ],
+                [HostRole.Id] = [
+                    CreatePropertyPermission.Id, ReadPropertyPermission.Id, UpdatePropertyPermission.Id, DeletePropertyPermission.Id,
+                    ReadBookingPermission.Id, UpdateBookingPermission.Id
+                ],
+                [GuestRole.Id] = [
+                    CreateBookingPermission.Id, ReadBookingPermission.Id, UpdateBookingPermission.Id,
+                    CreateReviewPermission.Id, ReadReviewPermission.Id
+                ]
+            };
+        }
+    }
+}
