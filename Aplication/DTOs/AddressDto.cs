f9842b88be03458140d81b360a2f538d4def3fb6
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-20 02:11:52 -0500

    Agregar Dto's para la creación de un huesped

---
+++ Aplication/DTOs/AddressDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace application.DTOs
+{
+    public class AddressDto
+    {
+        [Required(ErrorMessage = "Street is required")]
+        [MinLength(3, ErrorMessage = "Street must be at least 3 characters long")]
+        public required string Street { get; set; }
+
+        [Required(ErrorMessage = "Street number is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Street number must be greater than 0")]
+        public int StreetNumber { get; set; }
+
+        [Required(ErrorMessage = "Intersection number is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Intersection number must be greater than 0")]
+        public int IntersectionNumber { get; set; }
+
+        [Required(ErrorMessage = "Door number is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Door number must be greater than 0")]
+        public int DoorNumber { get; set; }
+
+        [Required(ErrorMessage = "City ID is required")]
+        public int CityId { get; set; }
+
+        [MaxLength(200, ErrorMessage = "Complement must be at most 200 characters long")]
+        public string? Complement { get; set; }
+
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+    }
+}
---
+++ Aplication/DTOs/GuestCreationDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace application.DTOs
+{
+    public class GuestCreationDto: UserCreationDto
+    {
+        [Required(ErrorMessage = "Address is required")]
+        public required AddressDto Address { get; set; }
+    }
+}
