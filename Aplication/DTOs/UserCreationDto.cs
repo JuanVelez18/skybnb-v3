c6eb434f82e0dfd6cf891a54a8a40656bf6917c5
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-22 07:38:19 -0500

    Agregar validaciones y errores a UserCreationDto

--- Aplication/DTOs/UserCreationDto.cs
+++ Aplication/DTOs/UserCreationDto.cs
@@ -1,20 +1,38 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static application.DTOs.UserCredentialsDto;
 
 namespace application.DTOs
 {
     public class UserCreationDto
     {
-        public required string Dni { get; set; }
-        public required string FirstName { get; set; }
-        public required string LastName { get; set; }
-        public required string Email { get; set; }
-        public required string Password { get; set; }
+        [Required(ErrorMessage = "Dni is required")]
+        public string Dni { get; set; }
+
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        [MaxLength(20, ErrorMessage = "Password must be at most 20 characters long")]
+        [RegularExpression(PasswordValidations.PasswordRegex, ErrorMessage = PasswordValidations.PasswordErrorMessage)]
+        public string Password { get; set; }
 
         [Required(ErrorMessage = "Birthday is required")]
-        public required DateOnly Birthday { get; set; }
+        public DateOnly Birthday { get; set; }
 
         [Required(ErrorMessage = "CountryId is required")]
-        public required int CountryId { get; set; }
+        public int CountryId { get; set; }
+
+        [MinLength(9, ErrorMessage = "Phone must be at least 8 characters long")]
+        [MaxLength(20, ErrorMessage = "Phone must be at most 20 characters long")]
+        [RegularExpression(@"^\d{9,20}$", ErrorMessage = "The phone must contain only numbers")]
         public string? Phone { get; set; }
 
         public UserCredentialsDto ToCredentials() => new()
