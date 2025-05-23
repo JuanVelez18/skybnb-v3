7a401f58ab3c3001eba5782e29a734e51e366843
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-22 21:06:00 -0500

    Agregar implementación de AuthPresentation y su interfaz IAuthPresentation para el registro de anfitriones y huéspedes

---
+++ presentations/Implementations/AuthPresentation.cs
@@ -0,0 +1,49 @@
+﻿using application.DTOs;
+using Microsoft.Extensions.Options;
+using presentations.Interfaces;
+
+namespace presentations.Implementations
+{
+    public class AuthPresentation : IAuthPresentation
+    {
+        private readonly PresentationConfiguration _configuration;
+        private readonly Comunication _comunication;
+
+        public AuthPresentation(IOptions<PresentationConfiguration> options)
+        {
+            _configuration = options.Value;
+            if (string.IsNullOrEmpty(_configuration.Host))
+                throw new ArgumentNullException(nameof(_configuration.Host), "Host cannot be null or empty.");
+
+            _comunication = new Comunication(_configuration.Host);
+        }
+
+        public async Task<TokensDto> RegisterHostAsync(UserCreationDto userCreationDto)
+        {
+            var response = await _comunication.Execute<TokensDto, UserCreationDto>("/auth/register/host", userCreationDto);
+            if (response.Ok)
+            {
+                _comunication.Authenticate(response.Data!.AccessToken, response.Data.RefreshToken);
+                return response.Data!;
+            }
+            else
+            {
+                throw new Exception($"Error: {response.Message}");
+            }
+        }
+
+        public async Task<TokensDto> RegisterGuestAsync(GuestCreationDto guestCreationDto)
+        {
+            var response = await _comunication.Execute<TokensDto, GuestCreationDto>("/auth/register/guest", guestCreationDto);
+            if (response.Ok)
+            {
+                _comunication.Authenticate(response.Data!.AccessToken, response.Data.RefreshToken);
+                return response.Data!;
+            }
+            else
+            {
+                throw new Exception($"Error: {response.Message}");
+            }
+        }
+    }
+}
---
+++ presentations/Interfaces/IAuthPresentation.cs
@@ -0,0 +1,10 @@
+﻿using application.DTOs;
+
+namespace presentations.Interfaces
+{
+    public interface IAuthPresentation
+    {
+        Task<TokensDto> RegisterHostAsync(UserCreationDto userCreationDto);
+        Task<TokensDto> RegisterGuestAsync(GuestCreationDto guestCreationDto);
+    }
+}
