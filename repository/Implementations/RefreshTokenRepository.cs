4ef7842590445fbaa8081a07cb0dbf9fc60d47d0
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-24 13:12:00 -0500

    Agregar RefreshTokenRepository y actualizar UnitOfWork para incluirlo

---
+++ repository/Implementations/RefreshTokenRepository.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+using repository.Conexions;
+using repository.Interfaces;
+
+namespace repository.Implementations
+{
+    public class RefreshTokenRepository: BaseRepository<RefreshTokens, long>, IRefreshTokenRepository
+    {
+        public RefreshTokenRepository(DbConexion conexion): base(conexion)
+        {
+        }
+    }
+}
