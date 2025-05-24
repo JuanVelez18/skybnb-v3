3c80713e2048d8530b8cbb33cf0070979959394d
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-24 14:53:39 -0500

    Implemente el método GetByHashedTokenAsync en RefreshTokenRepository

--- repository/Implementations/RefreshTokenRepository.cs
+++ repository/Implementations/RefreshTokenRepository.cs
@@ -1,13 +1,23 @@
 ﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using repository.Conexions;
 using repository.Interfaces;
 
 namespace repository.Implementations
 {
-    public class RefreshTokenRepository: BaseRepository<RefreshTokens, long>, IRefreshTokenRepository
+    public class RefreshTokenRepository : BaseRepository<RefreshTokens, long>, IRefreshTokenRepository
     {
-        public RefreshTokenRepository(DbConexion conexion): base(conexion)
+        public RefreshTokenRepository(DbConexion conexion) : base(conexion)
         {
+        }
+
+        public async Task<RefreshTokens?> GetByHashedTokenAsync(string hashedToken)
+        {
+            return await _dbSet
+                .Include(rt => rt.User)
+                .Include(rt => rt.ReplacedByToken)
+                .Include(rt => rt.ReplacesTokens)
+                .FirstOrDefaultAsync(rt => rt.TokenValue == hashedToken);
         }
     }
 }
--- repository/Interfaces/IRefreshTokenRepository.cs
+++ repository/Interfaces/IRefreshTokenRepository.cs
@@ -4,5 +4,6 @@ namespace repository.Interfaces
 {
     public interface IRefreshTokenRepository: IBaseRepository<RefreshTokens, long>
     {
+        Task<RefreshTokens?> GetByHashedTokenAsync(string hashedToken);
     }
 }
