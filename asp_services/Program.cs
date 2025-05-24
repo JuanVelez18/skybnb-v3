4d35426ba7c7cb1cc77d4e4f964d5c732d683277
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-24 09:05:51 -0500

    Implementar middleware de autenticación

--- asp_services/Program.cs
+++ asp_services/Program.cs
@@ -1,15 +1,25 @@
+using System.Text;
 using application.Core;
 using application.DTOs;
 using application.Implementations;
 using application.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using repository.Conexions;
 using repository.Configuration;
 using repository.Implementations;
 using repository.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var jwtSettings = builder.Configuration.GetSection("JwtOptions");
+string? secretKey = jwtSettings["SecretKey"];
+if (string.IsNullOrEmpty(secretKey) || Encoding.UTF8.GetBytes(secretKey).Length < 32)
+{
+    throw new InvalidOperationException("Please ensure that the 'JwtOptions:SecretKey' setting is present in the configuration and is at least 32 bytes long.");
+}
 
 var connectionString = builder.Configuration.GetConnectionString("ConexionString");
 
@@ -31,6 +41,25 @@ builder.Services.AddScoped<IDataInitializer, DataInitializer>();
 // Inyecta instancia de conexión a la base de datos
 builder.Services.AddDbContext<DbConexion>(options => options.UseSqlServer(connectionString));
 
+// Add authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = builder.Environment.IsProduction();
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+        ClockSkew = TimeSpan.FromSeconds(30)
+    };
+});
+
 var app = builder.Build();
 
 // Initialize the database
@@ -43,6 +72,7 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 
+// Authentication Middleware
 app.UseAuthorization();
 
 app.MapControllers();
