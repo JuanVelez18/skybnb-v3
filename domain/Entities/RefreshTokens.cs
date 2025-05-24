308b36ef625e627dca94782da44944ae22a11a71
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-24 12:31:16 -0500

    Agregar la entidad RefreshTokens y su migración correspondiente

---
+++ domain/Entities/RefreshTokens.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace domain.Entities
+{
+    public class RefreshTokens
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string TokenValue { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public DateTime IssuedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime ExpiresAt { get; set; }
+
+        public DateTime? RevokedAt { get; set; }
+
+        [Required]
+        public bool Used { get; set; } = false;
+
+        public int? ReplacedByTokenId { get; set; }
+
+
+        public Users? User { get; set; }
+        public RefreshTokens? ReplacedByToken { get; set; }
+        public List<RefreshTokens> ReplacesTokens { get; set; } = [];
+
+        [NotMapped]
+        public bool IsActive => RevokedAt == null && !Used && DateTime.UtcNow < ExpiresAt;
+    }
+}
