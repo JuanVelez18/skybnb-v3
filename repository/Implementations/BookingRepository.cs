0b23c5704d54ea0a1f233d4080fd705c2c2cf06f
Abdreah0 <andreahiguitagallego123@gmail.com>
2025-05-25 00:38:32 -0500

    Implementación e Interfaz del repositorio de Bookings

---
+++ repository/Implementations/BookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+using repository.Conexions;
+using repository.Interfaces;
+
+namespace repository.Implementations
+{
+    public class BookingRepository : BaseRepository<Bookings, Guid>, IBookingsRepository
+    {
+        public BookingRepository(DbConexion conexion) : base(conexion)
+        {
+        }
+    }
+}
