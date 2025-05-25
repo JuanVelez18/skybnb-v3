7fca0a1f34bdb871a0a58627a5c40038f6d299cf
Abdreah0 <andreahiguitagallego123@gmail.com>
2025-05-25 01:00:00 -0500

    Corrección en ReviewsDto

--- Aplication/DTOs/ReviewsDto.cs
+++ Aplication/DTOs/ReviewsDto.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace application.DTOs
 {
-    class ReviewsDto
+    public class ReviewsDto
     {
+        [Required(ErrorMessage = "Booking ID is required")]
+        public Guid BookingId { get; set; }
+
+        [Required(ErrorMessage = "Property ID is required")]
+        public Guid PropertyId { get; set; }
+
+        [Required(ErrorMessage = "Guest ID is required")]
+        public Guid GuestId { get; set; }
+
+        [Required(ErrorMessage = "Rating is required")]
+        [Range(1.0, 5.0, ErrorMessage = "Rating must be between 1 and 5")]
+        public decimal Rating { get; set; }
+
+        [Required(ErrorMessage = "Comment is required")]
+        [MaxLength(2000, ErrorMessage = "Comment must be at most 2000 characters long")]
+        public string Comment { get; set; }
     }
 }
