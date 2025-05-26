914dd8b32b10275c0cc3b5de62d7073e3a868757
Abdreah0 <andreahiguitagallego123@gmail.com>
2025-05-25 19:44:28 -0500

    Implementar aplicación Properties y su interfaz

---
+++ Aplication/Implementations/PropertiesApplication.cs
@@ -0,0 +1,66 @@
+﻿using application.Core;
+using application.DTOs;
+using application.Interfaces;
+using domain.Entities;
+using repository.Configuration;
+using repository.Interfaces;
+using System.Text.Json;
+
+namespace application.Implementations
+{
+    public class PropertiesApplication : IPropertiesApplication
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PropertiesApplication(
+            IUnitOfWork unitOfWork )
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task CreateProperties(PropertiesCreationDto propertiesCreationDto, Guid hostId)
+
+        {
+            var user = await _unitOfWork.Users.GetByIdAsync(hostId);
+
+            if (user == null)
+            {
+                throw new NotFoundApplicationException("User not found.");
+            }
+
+            if (!user.Roles.Any(r => r.Id == InitialData.HostRole.Id))
+            {
+                throw new UnauthorizedApplicationException("The user is not a host.");
+            }
+
+            var newProperty = new Properties(
+                propertiesCreationDto.Title,
+                propertiesCreationDto.Description!,
+                propertiesCreationDto.NumBathrooms,
+                propertiesCreationDto.NumBedrooms,
+                propertiesCreationDto.NumBeds,
+                propertiesCreationDto.MaxGuests,
+                propertiesCreationDto.BasePricePerNight,
+                propertiesCreationDto.TypeId,
+                propertiesCreationDto.HostId,
+                propertiesCreationDto.AddressId
+            );
+            await _unitOfWork.Properties.AddAsync(newProperty);
+
+            var auditory = new Auditories(
+                userId: hostId,
+                action: "Register Property",
+                entity: "Property",
+                entityId: newProperty.Id.ToString(),
+                details: JsonSerializer.Serialize(newProperty, new JsonSerializerOptions
+                {
+                    ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles
+                }),
+                timestamp: DateTime.UtcNow
+            );
+
+            await _unitOfWork.Auditories.AddAsync(auditory);
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
