a2e074d3928ec65e2939c90d37858fa477ebaae3
Abdreah0 <andreahiguitagallego123@gmail.com>
2025-05-26 16:52:11 -0500

    Implementar aplicacion para bookings y review

---
+++ Aplication/Implementations/BookingsApplication.cs
@@ -0,0 +1,59 @@
+﻿using application.Core;
+using application.DTOs;
+using application.Interfaces;
+using domain.Entities;
+using repository.Interfaces;
+using System.Text.Json;
+
+namespace application.Implementations
+{
+    public class BookingsApplication : IBookingsApplication
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BookingsApplication(
+            IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task CreateBooking(BookingsDto bookingDto, Guid userId)
+
+        {
+            var user = await _unitOfWork.Users.GetByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new NotFoundApplicationException("User not found.");
+            }
+
+            var newBooking = new Bookings(
+                propertyId: bookingDto.PropertyId,
+                guestId: bookingDto.GuestId,
+                checkInDate: bookingDto.CheckInDate,
+                checkOutDate: bookingDto.CheckOutDate,
+                numGuests: bookingDto.NumGuests,
+                totalPrice: bookingDto.TotalPrice
+        );
+            await _unitOfWork.Bookings.AddAsync(newBooking);
+
+
+            var auditory = new Auditories(
+                userId: userId,
+                action: "Create Booking",
+                entity: "Bookings",
+                entityId: newBooking.Id.ToString(),
+                details: JsonSerializer.Serialize(newBooking, new JsonSerializerOptions
+                {
+                    ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles
+                }),
+                timestamp: DateTime.UtcNow
+            );
+
+            await _unitOfWork.Auditories.AddAsync(auditory);
+
+            await _unitOfWork.CommitAsync();
+
+        }
+    }
+}
---
+++ Aplication/Implementations/ReviewsApplication.cs
@@ -0,0 +1,60 @@
+﻿
+using application.Core;
+using application.DTOs;
+using application.Interfaces;
+using domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using repository.Interfaces;
+using System.Text.Json;
+
+namespace application.Implementations
+{
+    public class ReviewsApplication : IReviewsApplication
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReviewsApplication(
+            IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task CreateReview(ReviewsDto reviewsDto, Guid userId)
+
+        {
+            var user = await _unitOfWork.Users.GetByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new NotFoundApplicationException("User not found.");
+            }
+
+            var newReview = new Reviews(
+                bookingId: reviewsDto.BookingId,
+                propertyId: reviewsDto.PropertyId,
+                guestId: reviewsDto.GuestId,
+                rating: reviewsDto.Rating,
+                comment: reviewsDto.Comment
+        );
+            await _unitOfWork.Reviews.AddAsync(newReview);
+
+
+            var auditory = new Auditories(
+                userId: userId,
+                action: "Create Review",
+                entity: "Reviews",
+                entityId: newReview.Id.ToString(),
+                details: JsonSerializer.Serialize(newReview, new JsonSerializerOptions
+                {
+                    ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles
+                }),
+                timestamp: DateTime.UtcNow
+            );
+
+            await _unitOfWork.Auditories.AddAsync(auditory);
+
+            await _unitOfWork.CommitAsync();
+
+        }
+    }
+}
