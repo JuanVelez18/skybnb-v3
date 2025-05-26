1642e1892960cf229d1e0b3952090bb2d78b4be1
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-26 18:11:58 -0500

    Implementar UserApplication e IUserApplication para la recuperación de resúmenes de usuario

---
+++ Aplication/Implementations/UserApplication.cs
@@ -0,0 +1,39 @@
+﻿using application.Core;
+using application.DTOs;
+using application.Interfaces;
+using repository.Interfaces;
+
+namespace application.Implementations
+{
+    public class UserApplication : IUserApplication
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserApplication(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<UserSummaryDto> GetUserSummaryByIdAsync(Guid id)
+        {
+            var user = await _unitOfWork.Users.GetByIdAsync(id);
+            if (user == null)
+            {
+                throw new NotFoundApplicationException($"User not found.");
+            }
+
+            var permissions = await _unitOfWork.Users.GetUserPermissionsAsync(id);
+
+            return new UserSummaryDto
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                CreatedAt = user.CreatedAt,
+                Roles = user.Roles?.Select(role => role.Name).ToList() ?? new List<string>(),
+                Permissions = permissions.Select(permission => permission.Name).ToList()
+            };
+        }
+    }
+}
