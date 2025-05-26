c14ccf17493cc89470ee3e747aace385dd37c85c
Abdreah0 <andreahiguitagallego123@gmail.com>
2025-05-25 20:55:53 -0500

    Implementar busqueda de usuarios por id con roles

--- repository/Interfaces/IUserRepository.cs
+++ repository/Interfaces/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace repository.Interfaces
     public interface IUserRepository : IBaseRepository<Users, Guid>
     {
         Task<Users?> GetByEmailAsync(string email);
+        new Task<Users?> GetByIdAsync(Guid id);
         void AssignRole(Users user, Roles role);
     }
 }
