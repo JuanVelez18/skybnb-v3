6adf493590ae859dab8a0f053a9c8802dbaf1e9f
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-27 02:22:43 -0500

    Agregar repositorio PropertyAssets y su integración en UnitOfWork

---
+++ repository/Implementations/PropertyAssetsRepository.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+using repository.Conexions;
+using repository.Interfaces;
+
+namespace repository.Implementations
+{
+    public class PropertyAssetsRepository : BaseRepository<PropertyAssets, Guid>, IPropertyAssetsRepository
+    {
+        public PropertyAssetsRepository(DbConexion conexion) : base(conexion)
+        {
+        }
+    }
+}
