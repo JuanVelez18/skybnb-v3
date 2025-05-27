4257d294c003626ef0a0ca27b610f489af3e9915
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-27 04:37:52 -0500

    Añadir servicio de creación de propiedades

--- web_presentation/Spa/src/queries/properties.queries.ts
+++ web_presentation/Spa/src/queries/properties.queries.ts
@@ -1,5 +1,16 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+import type { CreationMediaFile } from "@/models/multimedia";
+import type { PropertyBasicInformation } from "@/models/properties";
+import type { CreationAddress } from "@/models/ubication";
 import { PropertyService } from "@/services/property.service";
-import { useQuery } from "@tanstack/react-query";
+
+type CreationPropertyData = {
+  information: PropertyBasicInformation;
+  address: CreationAddress;
+  multimedia: CreationMediaFile[];
+};
 
 export const PropertiesQueryKeys = {
   all: ["properties"] as const,
@@ -16,5 +27,28 @@ export const useGetAllPropertyTypes = () => {
     propertyTypes: data,
     arePropertyTypesLoading: isLoading,
     isPropertyTypesError: isError,
+  };
+};
+
+export const useCreateProperty = () => {
+  const { mutate, isPending, isError } = useMutation({
+    mutationFn: (data: CreationPropertyData) =>
+      PropertyService.createProperty(
+        data.information,
+        data.address,
+        data.multimedia
+      ),
+    onSuccess() {
+      toast.success("Property created successfully!");
+    },
+    onError() {
+      toast.error("Failed to create property. Please try again.");
+    },
+  });
+
+  return {
+    createProperty: mutate,
+    isCreatingProperty: isPending,
+    isCreatePropertyError: isError,
   };
 };
