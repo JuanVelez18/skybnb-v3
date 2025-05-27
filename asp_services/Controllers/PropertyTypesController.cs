14a57df8f6d99f3414c64d9f4cc4fb639286f715
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-26 22:05:04 -0500

    Implementar endpoint `/property-types` para lista todos los tipos de propiedad

---
+++ asp_services/Controllers/PropertyTypesController.cs
@@ -0,0 +1,24 @@
+﻿using application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_services.Controllers
+{
+    [ApiController]
+    [Route("api/property-types")]
+    public class PropertyTypesController : ControllerBase
+    {
+        private readonly IPropertiesApplication _propertiesApplication;
+
+        public PropertyTypesController(IPropertiesApplication propertiesApplication)
+        {
+            _propertiesApplication = propertiesApplication;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPropertyTypes()
+        {
+            var propertyTypes = await _propertiesApplication.GetAllPropertyTypesAsync();
+            return Ok(propertyTypes);
+        }
+    }
+}
