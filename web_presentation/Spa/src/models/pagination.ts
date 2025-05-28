f1c3b31bfdcabd22f75c5f1a894036c815c653d4
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-28 12:02:46 -0500

    SPA: Refactorizar los tipos de paginación y agregar la función de conversión a DTO

--- web_presentation/Spa/src/models/pagination.ts
+++ web_presentation/Spa/src/models/pagination.ts
@@ -9,3 +9,19 @@ export type PaginationOptions = {
   page?: number;
   pageSize?: number;
 };
+
+export type PageDto<T> = {
+  results: T[];
+  page: number;
+  totalPages: number;
+  totalCount: number;
+};
+
+export function dtoToPage<T>(dto: PageDto<T>): Page<T> {
+  return {
+    results: dto.results,
+    total: dto.totalCount,
+    page: dto.page,
+    totalPages: dto.totalPages,
+  };
+}
