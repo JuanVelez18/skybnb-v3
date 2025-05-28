aec390d8d478fa5a609905b6963fb8f2a88ab700
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-05-27 22:30:41 -0500

    Agregar clases Page y PaginationOptions para manejar la paginación de datos

---
+++ domain/Core/Page.cs
@@ -0,0 +1,11 @@
+﻿namespace domain.Core
+{
+    public class Page<T> where T : class
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public required long TotalCount { get; set; }
+        public required int CurrentPage { get; set; }
+        public required int PageSize { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    }
+}
---
+++ domain/Core/PaginationOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace domain.Core
+{
+    public class PaginationOptions
+    {
+        public int PageNumber { get; set; } = 1;
+        private int _PageSize { get; set; } = 10;
+        private const int MaxPageSize = 50;
+
+        public int PageSize
+        {
+            get => _PageSize;
+            set => _PageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
