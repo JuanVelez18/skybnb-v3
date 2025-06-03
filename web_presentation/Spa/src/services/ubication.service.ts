522440b9bc0a4101b776e39f08551063b49d708d
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-06-03 11:58:01 -0500

    Añadir consultas para obtener todos los países

--- web_presentation/Spa/src/services/ubication.service.ts
+++ web_presentation/Spa/src/services/ubication.service.ts
@@ -1,10 +1,21 @@
 import httpClient from "@/core/httpClient";
-import type { City } from "@/models/ubication";
+import {
+  dtoToCountry,
+  type City,
+  type Country,
+  type CountryDto,
+} from "@/models/ubication";
 
 export class UbicationService {
   public static async getAllCities(): Promise<City[]> {
     const response = await httpClient.get<City[]>("/cities");
 
     return response.data;
+  }
+
+  public static async getAllCountries(): Promise<Country[]> {
+    const response = await httpClient.get<CountryDto[]>("/countries");
+
+    return response.data.map(dtoToCountry);
   }
 }
