19797cbb1192a4b8a55a29fdcd9de592ec7d721b
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-06-04 11:34:53 -0500

    Agregar archivo index.ts para exportar componentes de tarjetas

---
+++ web_presentation/Spa/src/components/cards/index.ts
@@ -0,0 +1,4 @@
+export { default as LocationCard } from "./LocationCard.tsx";
+export { default as PropertyCard } from "./PropertyCard.tsx";
+export { default as PropertyHostCard } from "./PropertyHostCard.tsx";
+export { default as PropertyInfoDetailCard } from "./PropertyInfoDetailCard.tsx";
