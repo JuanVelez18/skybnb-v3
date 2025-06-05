864e5665b18a22c278d6f221043136178699af4f
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-06-05 00:32:53 -0500

    SPA: Mover la inicialización de sesión al archivo main.tsx y eliminar el uso de useEffect en App.tsx

--- web_presentation/Spa/main.tsx
+++ web_presentation/Spa/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { initializeSession } from "@/utils/auth.ts";
 import App from "./App.tsx";
 
 import "./index.css";
 
+initializeSession();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
