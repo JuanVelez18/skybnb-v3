24c02b69c730f8e6f9a012550f9949cc92e92686
Juan Velez <juanvelez1113670@correo.itm.edu.co>
2025-06-05 02:03:54 -0500

    SPA: Implementar método GetBookingsByUserIdAsync en BookingRepository con filtrado y paginado

--- repository/Implementations/BookingRepository.cs
+++ repository/Implementations/BookingRepository.cs
@@ -1,4 +1,5 @@
-﻿using domain.Entities;
+﻿using domain.Core;
+using domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using repository.Conexions;
 using repository.Interfaces;
@@ -9,6 +10,53 @@ namespace repository.Implementations
     {
         public BookingRepository(DbConexion conexion) : base(conexion)
         {
+        }
+
+        public async Task<Page<Bookings>> GetBookingsByUserIdAsync(Guid userId, PaginationOptions pagination, BookingFilters? filters)
+        {
+            IQueryable<Bookings> query = _dbSet
+                .AsNoTracking()
+                .Include(b => b.Property)
+                    .ThenInclude(p => p!.Host)
+                .Include(b => b.Property)
+                    .ThenInclude(p => p!.City)
+                .Include(b => b.Property)
+                    .ThenInclude(p => p!.Country)
+                .Include(b => b.Property)
+                    .ThenInclude(p => p!.Multimedia)
+                .Include(b => b.Guest)
+                .Include(b => b.Review)
+                .Where(b => b.GuestId == userId || (b.Property != null && b.Property.HostId == userId));
+
+            if (filters != null)
+            {
+                foreach (var filter in filters.GetFilterExpressions(userId))
+                {
+                    query = query.Where(filter);
+                }
+
+                var sortExpression = filters.GetSortExpression();
+                if (sortExpression != null)
+                {
+                    query = filters.IsSortDescending()
+                        ? query.OrderByDescending(sortExpression)
+                        : query.OrderBy(sortExpression);
+                }
+            }
+
+            long total = await query.CountAsync();
+            List<Bookings> bookings = await query
+                .Skip((int)((pagination.PageNumber - 1) * pagination.PageSize))
+                .Take(pagination.PageSize)
+                .ToListAsync();
+
+            return new Page<Bookings>
+            {
+                Items = bookings,
+                TotalCount = total,
+                CurrentPage = pagination.PageNumber,
+                PageSize = pagination.PageSize
+            };
         }
 
         public async Task<List<Bookings>> GetConfirmedBookingsByPropertyIdAsync(Guid propertyId)
--- repository/Interfaces/IBookingsRepository.cs
+++ repository/Interfaces/IBookingsRepository.cs
@@ -1,8 +1,11 @@
-﻿using domain.Entities;
+﻿using domain.Core;
+using domain.Entities;
+
 namespace repository.Interfaces
 {
     public interface IBookingsRepository : IBaseRepository<Bookings, Guid>
     {
+        Task<Page<Bookings>> GetBookingsByUserIdAsync(Guid userId, PaginationOptions pagination, BookingFilters? filters);
         Task<List<Bookings>> GetConfirmedBookingsByPropertyIdAsync(Guid propertyId);
         Task<List<Bookings>> GetPendingBookingsByGuestIdAsync(Guid guestId);
     }
